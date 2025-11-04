37043c183be062302d2d196c9c572725a70f0331
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-11-04 16:28:57 +0100

    added homecontroller class

---
+++ src/main/java/project/bioinformatics/controller/HomeController.java
@@ -0,0 +1,12 @@
+package project.bioinformatics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Backend działa poprawnie 🚀";
+    }
+}
